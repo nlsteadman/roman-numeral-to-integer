afadbcc670bf6c2fce019f9cd32d93d5565e45d6
Nicole Steadman <nsteadman@gmail.com>
2022-10-18 11:48:36 -0700

    solved

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,29 @@
 function romanNumeral(string) {
-  // type your code here
+
+  const numerals = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  }
+
+  let result = 0;
+
+  for (i = 0; i < string.length; i++) {
+    const current = numerals[string[i]];
+    const next = numerals[string[i + 1]];
+
+    if (current < next) {
+      result += (next - current);
+      i++
+    } else {
+      result += current
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
